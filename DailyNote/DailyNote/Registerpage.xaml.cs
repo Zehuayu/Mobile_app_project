0bf1f78ad92ba6aedbdf65a7a4b34a9403635df3
ZehuaYu <yuzehua@gmail.com>
2017-03-22 16:29:55 +0000

    new push

---
+++ DailyNote/DailyNote/Registerpage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Storage;
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DailyNote
+{
+   
+    public sealed partial class Registerpage : Page
+    {
+
+
+
+      
+        public Registerpage()
+        {
+            this.InitializeComponent();
+        }
+
+
+        [Table("userinfo")]
+        public class userinfo
+        {
+               
+            public string username { get; set; }
+            public string password { get; set; }
+        }
+
+
+
+        private void register(String Username, String password) {
+           Sq
+        }
+
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var name = textBox.Text;
+            var password = textBox1.Text;
+            register(name, password);
+        }
+    }
+}
---
+++ DailyNote/DailyNote/SQLiteCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SQLitePCL;
+
+namespace DailyNote
+{
+    internal class SQLiteCommand
+    {
+        private SQLiteConnection conn;
+        private string sql;
+
+        public SQLiteCommand(SQLiteConnection conn)
+        {
+            this.conn = conn;
+        }
+
+        public SQLiteCommand(string sql, SQLiteConnection conn)
+        {
+            this.sql = sql;
+            this.conn = conn;
+        }
+
+        public string CommandText { get; internal set; }
+
+        internal void ExecuteNonQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
